3580b0c29fc121299d90e468bd5d0bae3ac95541
jAlvarosf <jose.alvaro.sf@hotmail.com>
2021-02-07 01:06:21 -0300

    correções de português no html

--- resources/views/authentication-login.blade.php
+++ resources/views/authentication-login.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <!-- Icone Favicon -->
+    <!-- Ícone Favicon -->
     <link rel="icon" type="image/png" sizes="16x16" href="../../assets/images/favicon.png">
     <title>AtosSystem - Sistema de Gestão para Igrejas </title>
     <!-- Custom CSS -->
